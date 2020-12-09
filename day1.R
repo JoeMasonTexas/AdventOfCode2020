4816181ad2c0de58922520f65a31d8d64101156c
JoeMasonTexas <joemason@utexas.edu>
2020-12-09 09:18:27 -0500

    Completed part 2 of the Day 1 Challenge

--- day1.R
+++ day1.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 
+# read in the data
 day1 <- read_lines('day1Data.txt') %>%
   as.numeric()
 
+# part 1
 for(i in 1:length(day1)) {
   
   needed <- (2020 - day1[[i]])
@@ -13,6 +15,24 @@ for(i in 1:length(day1)) {
       print(day1[[j]])
       print(day1[[i]]*day1[[j]])
       break()
+    }
+  }
+}
+
+# part 2
+for(i in 1:length(day1)) {
+  
+  needed1 <- (2020 - day1[[i]])
+  
+  for(j in 1:length(day1)) {
+    needed2 <- needed1 - day1[[j]]
+    for(k in j:length(day1)) {
+      if(needed2 == day1[[k]]){
+        print(day1[[i]])
+        print(day1[[j]])
+        print(day1[[k]])
+        print(day1[[i]]*day1[[j]]*day1[[k]])
+      }
     }
   }
 }
